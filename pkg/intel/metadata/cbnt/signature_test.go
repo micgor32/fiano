afa38e5806a65cc54796e6886a4d2bd602206c7e
Michal Gorlas <michal.gorlas@9elements.com>
2026-03-19 19:10:47 +0100

    chore(cbnt): add license headers to the test files
    Signed-off-by: Michal Gorlas <michal.gorlas@9elements.com>

--- pkg/intel/metadata/cbnt/signature_test.go
+++ pkg/intel/metadata/cbnt/signature_test.go
@@ -1,3 +1,7 @@
+// Copyright 2026 the LinuxBoot Authors. All rights reserved
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
 package cbnt
 
 import (
